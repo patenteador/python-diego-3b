2da49787656db159689aa916de6a7bcccc8a06e3
Diego Luan <108006120+patenteador@users.noreply.github.com>
2024-09-12 10:36:55 -0300

    aula 12/09

---
+++ app.py
@@ -0,0 +1,40 @@
+import os   
+
+def mostra_titulo ():
+print ('mercearia do ze')
+
+
+def mostra_escolhas ()
+print ('1. Cadastro de produtos e mercadorias')
+print ('2. Listar produtos e mercadorias')
+print ('3. Ativar produtos e mercadorias')
+print ('4. Sair da aplicação')
+
+
+def escolhe_opcao()
+    opcao_escolhida = int (input ( 'Escolha uma opção: ') )
+    print ('Você escolheu a opção: ', opcao_escolhida)
+
+def finalizar_programa ( ) :
+        os.system ('clear')    
+        print(' Finalizando programa')
+        if opcao_escolhida == 1:
+            print('Cadastrar nadador')
+        elif opcao_escolhida == 2:    
+            print('listar nadadores')
+        elif opcao_escolhida == 3:
+            print
+        else
+            finalizar_programa()
+ 
+ def main() :
+    mostra_titulo()
+    mostra_escolhas()
+    escolhe_opcao()
+
+if __name__ == 'm '
+
+
+
+
+
